78ec07c09419f1f12d35ee4fd12e98d9733b68a6
frannyfra <francescadelaurentis1987@gmail.com>
2019-07-24 13:01:45 +0100

    set up server on port 3000

---
+++ src/router.js
@@ -0,0 +1,11 @@
+const router = (resquest, response) => {
+    const endpoint = request.url;
+
+    console.log('this is your url: ', endpoint);
+
+    // if (endpoint === '/') {
+
+    // } else if (endpoint)
+}
+
+module.exports = router;
---
+++ src/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const router = require('./router');
+const server = http.createServer(router); 
+
+let port = process.env.PORT || 3000;
+server.listen(port);
+
+
+console.log(`server up and running on port: ${port}`);
