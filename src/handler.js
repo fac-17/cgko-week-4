c432f04f2dcac3eab72b0e4f342843132ccffc5e
tonylomax <antl.lomax@gmail.com>
2019-07-24 16:16:08 +0100

    function/handler-endpoints

---
+++ src/handler.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const querystring = require("querystring");
+const path = require("path");
+
+function handleHome(request, response, endpoint) {
+  const filePath = path.join(__dirname, "..", "public/index.html");
+  fs.readFile(filePath, (error, file) => {
+    if (error) {
+      console.log(error);
+      response.writeHead(500, { "content-type": "text/html" });
+      response.end("<h1>Sorry! We had a problem on our end</h1>");
+    } else {
+      response.writeHead(200, { "content-type": "text/html" });
+      response.end(file);
+    }
+  });
+}
+
+function handlePublic(request, response, endpoint) {
+  const extension = endpoint.split(".")[1];
+
+  const extensionType = {
+    css: "text/css",
+    js: "text/js",
+    ico: "image/x-icon"
+  };
+  const filePath = path.join(__dirname, "..", endpoint);
+  fs.readFile(filePath, (error, file) => {
+    if (error) {
+      console.log(error);
+    } else {
+      response.writeHead(200, { "content-type": extensionType[extension] });
+      response.end(file);
+    }
+  });
+}
+
+module.exports = { handleHome, handlePublic };
--- src/router.js
+++ src/router.js
@@ -1,11 +1,22 @@
+const { handleHome, handlePublic } = require("../src/handler");
+
 const router = (request, response) => {
   const endpoint = request.url;
-
-  // console.log('this is your url: ', endpoint);
-  response.statusCode = 200;
-  response.setHeader("Content-Type", "text/html");
-  response.end("<h1>Hello World</h1>");
+  if (endpoint === "/") {
+    handleHome(request, response, endpoint);
+  } else if (endpoint.indexOf("public") !== -1) {
+    handlePublic(request, response, endpoint);
+  } else {
+    response.writeHead(404);
+    response.end("404 - PAGE NOT FOUND");
+  }
 };
+
+// response.statusCode = 200;
+// response.setHeader("Content-Type", "text/html");
+// response.end("<h1>Hello World</h1>");
+
+//call handler(request, response, endpoint)
 
 // if (endpoint === '/') {
 
