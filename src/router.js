6d15fcb29baac24b68230baad9bbf6edfd7f8ae8
Christine <christine.altmann@gmx.com>
2019-07-24 14:30:30 +0100

    set up server

--- src/router.js
+++ src/router.js
@@ -1,11 +1,14 @@
 const router = (resquest, response) => {
-    const endpoint = request.url;
+  const endpoint = request.url;
 
-    console.log('this is your url: ', endpoint);
+  // console.log('this is your url: ', endpoint);
+  response.statusCode = 200;
+  response.setHeader("Content-Type", "text/html");
+  response.end("<h1>Hello World</h1>");
+};
 
-    // if (endpoint === '/') {
+// if (endpoint === '/') {
 
-    // } else if (endpoint)
-}
+// } else if (endpoint)
 
 module.exports = router;
